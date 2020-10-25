be0823c59cde1415cb8e5f1d218aac30c35a304a
Sofía Lay <sofi-lay@hotmail.com>
2020-10-25 20:00:19 -0300

    filter terminado

--- Desktop/ADA_TP3_DOM/index.js
+++ Desktop/ADA_TP3_DOM/index.js
@@ -32,13 +32,17 @@ const getUsers = async () => {
         .then(res => {
             users = res.data;
             createTable(users);
+            filter(users);
         })
         .catch(err => alert("Hubo un error"));
 }
 
+
 const createTable = (users) => {
 
     const tbody = document.querySelector("#table-body");
+
+    tbody.innerHTML = ''
 
     users.forEach(user => {
         const row = document.createElement("tr");
@@ -83,154 +87,27 @@ const createTable = (users) => {
         tbody.appendChild(row);
 
     })
-
-    const search = () => {
-        const inputFilter = document.querySelector("#search"); 
-        
-        inputFilter.addEventListener("keyup", e => {
-            const buscador = inputFilter.value.toLowerCase();
-            console.log('resultado del buscador', buscador);
-            // if (buscador.length < 3) return;
-    
-             users.filter(user => {
-                if(
-                user.fullname.toLowerCase().includes(buscador) 
-             ){
-                return true;
-             }
-            });
-            // console.log('resultados', resultados)
-        })
-    }
-
-    search()
 }
 
-// let userFilter;
+const filter = (users) => {
+    const buscador = document.querySelector("#search");
 
-
-
-
-
-//Ejemplo Codepen:
-
-// const search_input = document.getElementById('search');
-// const results = document.getElementById('results');
-
-// let search_term = '';
-// let countries;
-
-// const fetchCountries = async () => {
-// 	countries = await fetch('https://restcountries.eu/rest/v2/all?fields=name;population;flag').then(
-// 		res => res.json()
-// 	);
-// }
-
-// const showCountries = async () => {
-// 	// clearHTML
-// 	results.innerHTML = '';
-	
-// 	// getting the data
-// 	await fetchCountries();
-	
-// 	// creating the structure
-// 	const ul = document.createElement("ul");
-// 	ul.classList.add('countries');
-	
-// 	countries.filter(
-// 		country => country.name.toLowerCase().includes(search_term.toLowerCase())
-// 	).forEach(country => {
-// 		const li = document.createElement('li');
-// 		const country_flag = document.createElement('img');
-// 		const country_name = document.createElement('h3');
-// 		const country_info = document.createElement('div');
-// 		const country_population = document.createElement('h2');
-// 		const country_popupation_text = document.createElement('h5');
-		
-// 		li.classList.add('country-item');
-// 		country_info.classList.add('country-info');
-		
-// 		country_flag.src = country.flag;
-// 		country_flag.classList.add('country-flag');
-		
-// 		country_name.innerText = country.name;
-// 		country_name.classList.add('country-name');
-		
-// 		country_population.innerText = numberWithCommas(country.population);
-// 		country_population.classList.add('country-population');
-// 		country_popupation_text.innerText = 'Population';
-// 		country_popupation_text.classList.add('country-population-text');
-		
-// 		country_info.appendChild(country_population);
-// 		country_info.appendChild(country_popupation_text);
-		
-// 		li.appendChild(country_flag);
-// 		li.appendChild(country_name);
-// 		li.appendChild(country_info);
-// 		ul.appendChild(li);
-// 	})
-// 	results.appendChild(ul);
-// }
-
-// // display initial countries
-// showCountries();
-
-// search_input.addEventListener('input', (e) => {
-// 	search_term = e.target.value;
-// 	// re-display countries again based on the new search_term
-// 	showCountries();
-// });
-
-
-
-
-
-    //Ejemplo de Fede
-
-    //inputFilter = document.querySelector("#search").value.toLowerCase()
-    // inputFilter.addEventListener('keyup', (e) => {
-    //     console.log('pepito')
-    // })
-
-    // const resultados = datos.filter(item => {
-    //     if (
-    //         item.nombre.toLowerCase().indexOf(inputador) > -1 ||
-    //         String(item.nivel).indexOf(inputador) > -1
-    //     ) {
-    //         return true;
-    //     }
-    //     return false;
-    // });
-
-/* API */
-/* https://5f7c70d600bd74001690ac5e.mockapi.io/users */
-
-//// Ejemplo del filter 
-/* const onload = () => {
-    const datos = [
-        { nombre: "Goku", nivel: 9999 },
-        { nombre: "Gohan", nivel: 150000 },
-        { nombre: "Vegeta", nivel: 9955 }
-    ];
-    const buscador = document.querySelector("#buscador");
-    buscador.addEventListener("click", () => {
-        const inputador = document.querySelector("#inputador").value.toLowerCase();
-        if (inputador.length < 3) return;
-        const resultados = datos.filter(item => {
-            if (
-                item.nombre.toLowerCase().indexOf(inputador) > -1 ||
-                String(item.nivel).indexOf(inputador) > -1
-            ) {
+    buscador.addEventListener('keyup', e => {
+        const inputFilter = buscador.value.toLowerCase();
+         
+        const resultados = users.filter(user => {
+            if(
+                user.fullname.toLowerCase().includes(inputFilter)
+            ){
                 return true;
+            }else {
+                return false;
             }
-            return false;
-        });
-        const resultador = document.querySelector("#resultador");
-        let text = ``;
-        for (let res of resultados) {
-            text += `${res.nombre} - nivel ${res.nivel}
-            `;
-        }
-        resultador.value = text;
-    });
-} */
+        })
+        createTable(resultados);
+    })
+    }
+
+    filter(users);
+
+    
