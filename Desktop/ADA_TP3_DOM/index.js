4dac67599099138c713ec63b4fc43bc25a47f30f
Moraima <moraimarp@gmail.com>
2020-10-24 23:48:08 -0500

    Add, edit, delete funcionando con modal reciclado

--- Desktop/ADA_TP3_DOM/index.js
+++ Desktop/ADA_TP3_DOM/index.js
@@ -1,46 +1,122 @@
+// Validar Inputs
+const validateEmail = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validatePhone = (phone) => {
+    const re = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g
+    return re.test(String(phone).toLowerCase());
+}
+
+const checkAll = () => {
+    const checks = document.querySelectorAll("#checkBoxId");
+    checks.forEach(check => check.checked = !check.checked);
+}
+
 const load = () => {
+    // Add New Employee
+    document.querySelector('#btn-add').addEventListener('click', () => {
+        let isValid = true;
+        const newName = document.querySelector("#name").value;
+        if (!newName || newName.length > 50) {
+            alert("Name: Not valid.");
+            isValid = false;
+        }
+        const newEmail = document.querySelector("#email").value;
+        if (!validateEmail(newEmail)) {
+            alert("Email: Not valid.");
+            isValid = false;
+        }
+        const newAddress = document.querySelector("#address").value;
+        if (!newAddress || newAddress.length > 60) {
+            alert("Address: Not valid.");
+            isValid = false;
+        }
+        const newPhone = document.querySelector("#phone").value;
+        if (!validatePhone(newPhone)) {
+            alert("Phone: Not valid.");
+            isValid = false;
+        }
+        const newUser = {
+            phone: newPhone,
+            email: newEmail,
+            address: newAddress,
+            fullname: newName,
+        }
+        if (isValid) {
+            createUser(newUser);
+            document.querySelector("#name").value= '';
+            document.querySelector("#email").value = '';
+            document.querySelector("#address").value = '';
+            document.querySelector("#phone").value = '';
 
-    ///// EMPEZAMOS CON TABLA//////
+            // Cerrar modal
+            closebox('cerrarbox'); // no recuerdo si hay otra forma de pasar un parametro statico
+        }
+        
+    });
+
+    document.querySelector("#table-checkbox").addEventListener("click", () => { checkAll(); });
+
+    // Cargar datos
     getUsers();
-
 }
 
 const getUsers = async () => {
     try {
-        const res = await axios.get(`https://5f7c70d600bd74001690ac5e.mockapi.io/users/`)
-        const users = res.data;
-        console.log(res.data)
-        createTable(users);
+        const res = await axios.get(`https://5f7c70d600bd74001690ac5e.mockapi.io/users`);
+        createTable(res.data);
     } catch (err) {
         console.error(err, `que pasa`);
     }
 }
 
+const editUsers = async (editUser, id) => { // newUser se podria llamar editedUser y pasar solo id del usuario a modificar
+    try {
+        await axios.put(`https://5f7c70d600bd74001690ac5e.mockapi.io/users/${id}`, editUser);
+        getUsers();
+    } catch (err) {
+        console.log(err, `edito?`);
+    }
+}
+
+const createUser = async (newUser) => {
+    try {
+        await axios.post(`https://5f7c70d600bd74001690ac5e.mockapi.io/users`, newUser);
+        getUsers();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 const createTable = (users) => {
     const tbody = document.querySelector("#table-body");
+    tbody.innerHTML = '';
     users.forEach(user => {
         const row = document.createElement("tr");
-        row.classList.add("table")
+        row.classList.add("table");
         row.setAttribute('id',`tableid${user.id}`);
         const tdCheck = document.createElement("td");
         tdCheck.classList.add("check");
         const check = document.createElement("input");
         check.type = "checkbox";
+        check.id = "checkBoxId";
+        check.checked = false;
         const tdName = document.createElement("td");
-        tdName.classList.add("table-content");
+        tdName.classList.add("modal-footer");
         const tdEmail = document.createElement("td");
-        tdEmail.classList.add("table-content");
+        tdEmail.classList.add("modal-footer");
         const tdAddress = document.createElement("td");
-        tdAddress.classList.add("table-content");
+        tdAddress.classList.add("modal-footer");
         const tdPhone = document.createElement("td");
-        tdPhone.classList.add("table-content");
+        tdPhone.classList.add("modal-footer");
         const tdActions = document.createElement("td");
-        tdActions.classList.add("table-content");
+        tdActions.classList.add("modal-footer");
         const btnEdit = document.createElement("button");
-        btnEdit.classList.add("btn");
+        btnEdit.classList.add("edit");
         const btnDelete = document.createElement("button");
-        btnDelete.classList.add("btn");
-        btnDelete.setAttribute('id', 'btn-delete');
+        btnDelete.classList.add("delete");
 
         tdName.innerText = user.fullname;
         tdEmail.innerText = user.email;
@@ -59,7 +135,37 @@ const createTable = (users) => {
         tdActions.appendChild(btnEdit);
         tdActions.appendChild(btnDelete);
         tbody.appendChild(row);
-        console.log(tbody)
+
+        const edit = user => {
+            
+            // Abrir Edit Modal
+            openbox('editbox');
+
+            document.querySelector("#name2").value = user.fullname;
+            document.querySelector("#email2").value = user.email;
+            document.querySelector("#address2").value = user.address;
+            document.querySelector("#phone2").value = user.phone;
+            document.querySelector("#btn-edit2").addEventListener('click', () => {
+                const editName = document.querySelector("#name2").value;
+                const editEmail = document.querySelector("#email2").value;
+                const editAddress = document.querySelector("#address2").value;
+                const editPhone = document.querySelector("#phone2").value;
+
+                const editUser = {
+                    ...user,
+                    phone: editPhone,
+                    email: editEmail,
+                    address: editAddress,
+                    fullname: editName,
+                }
+
+                editUsers(editUser, user.id);
+
+                // Cerrar modal
+                closebox('cerrarbox'); // no recuerdo si hay otra forma de pasar un parametro estatico
+            });
+        }
+        btnEdit.addEventListener("click", () => edit(user));
 
         // Proceso para Eliminar Employee
         const delEmployee = user => {   
@@ -79,44 +185,49 @@ const createTable = (users) => {
         };
         btnDelete.addEventListener('click', () => delEmployee(user));
         // Termina proceso de Eliminar Employee
-    });
 
+    });
 }
 
 
-
-// Abrir y Cerrar Modals
+// Abrir modals
 const openbox = (modalType) => {
     //console.log("openbox");
     const modal = document.querySelector('#modal');
+    const modalEdit = document.querySelector('#edit-modal');
     const backgroundModal = document.querySelector('.background-modal')
-    
-    backgroundModal.style.display = "block";
-    modal.style.top = "0";
-    modal.style.right = "50";
-}
-
-const closebox = (modalType) => {
-    const backgroundModal = document.querySelector('.background-modal');
-    const modalAdd = document.querySelector('#modal');
-    const modalDelete= document.querySelector('#modal-del');
     
     switch(modalType) {
         case 'addbox':
-            //console.log("delbox");
+            backgroundModal.style.display = "block";
+            modal.style.top = "0";
+            break;
+        case 'editbox':
+            backgroundModal.style.display = "block";
+            modalEdit.style.top = "0";
+            break;
+    }    
+}
+
+// Cerrar modals
+const closebox = (modalType) => {
+    const backgroundModal = document.querySelector('.background-modal');
+    const modalAdd = document.querySelector('#modal');
+    const modalEdit= document.querySelector('#edit-modal');
+    
+    switch(modalType) {
+        case 'addbox':
             backgroundModal.style.display = "none";
             modalAdd.style.top = "-1000px";
             break;
-        case 'delbox':
-            //console.log("delbox");
+        case 'editbox':
             backgroundModal.style.display = "none";
-            modalDelete.style.top = "-1000px";
+            modalEdit.style.top = "-1000px";
             break;
-        case 'cancelbox':
-            //console.log("closebox");
+        case 'cerrarbox':
             backgroundModal.style.display = "none";
             modalAdd.style.top = "-1000px";
-            modalDelete.style.top = "-1000px";
+            modalEdit.style.top = "-1000px";
             break;
     }
 
@@ -126,10 +237,10 @@ const closebox = (modalType) => {
 window.onclick = (event) => {
     const backgroundModal = document.querySelector('.background-modal');
     const modalAdd = document.querySelector('#modal');
-    const modalDelete= document.querySelector('#modal-del');
+    const modalEdit= document.querySelector('#edit-modal');
     if (event.target === backgroundModal) {
         backgroundModal.style.display = "none";
         modalAdd.style.top = "-1000px";
-        modalDelete.style.top = "-1000px";
+        modalEdit.style.top = "-1000px";
     }
 }
